eb5104eb05f77d290795e86ef0a8f58345398d77
Victor Toso <victortoso@redhat.com>
2023-07-04 20:26:21 +0200

    hooks: do not panic()
    "Should not happen" does not mean it can't happen. Do not panic!
    
    Signed-off-by: Victor Toso <victortoso@redhat.com>

--- pkg/hooks/manager.go
+++ pkg/hooks/manager.go
@@ -348,7 +348,7 @@ func (m *hookManager) PreCloudInitIso(vmi *v1.VirtualMachineInstance, cloudInitD
 			}
 			return preCloudInitIsoValidateResult(cloudInitData.DataSource, result.GetCloudInitData(), result.GetCloudInitNoCloudSource())
 		default:
-			panic("Should never happen, version compatibility check is done during Info call")
+			log.Log.Errorf("Unsupported callback version: %s", callback.Version)
 		}
 	}
 	return cloudInitData, nil
