62224c01b49d265d6338d62ebcec719167679da8
Victor Toso <victortoso@redhat.com>
2023-07-10 16:37:10 +0200

    fixup: add rpm
    Signed-off-by: Victor Toso <victortoso@redhat.com>

--- hack/rpm-deps.sh
+++ hack/rpm-deps.sh
@@ -91,12 +91,12 @@ launcherbase_main="
   libvirt-daemon-driver-qemu-${LIBVIRT_VERSION}
   passt-${PASST_VERSION}
   qemu-kvm-core-${QEMU_VERSION}
+  qemu-kvm-device-usb-host-${QEMU_VERSION}
   swtpm-tools-${SWTPM_VERSION}
 "
 launcherbase_x86_64="
   edk2-ovmf-${EDK2_VERSION}
   qemu-kvm-device-usb-redirect-${QEMU_VERSION}
-  qemu-kvm-device-usb-host-${QEMU_VERSION}
   seabios-${SEABIOS_VERSION}
 "
 launcherbase_aarch64="
