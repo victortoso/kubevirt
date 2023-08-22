84a4fdefa346d2ed31b2da243e0a7f0362af1b86
Victor Toso <victortoso@redhat.com>
2023-08-22 06:16:29 +0200

    sidecar-shim: add support for PreCloudInitIso
    The introduction of sidecar-shim base image only supported
    OnDefineDomain hook point. This patch adds support to PreCloudInitIso.
    
    From the PreCloudInitIsoParams, we ignore member
    CloudInitNoCloudSource as its considered deprecated "New code should
    use cloudInitData instead", so only CloudInitData member is passed.
    
    Similarly to OnDefineDomain, we pass the args as flags. Follow up
    commit will update the documentation.
    
    Signed-off-by: Victor Toso <victortoso@redhat.com>

--- cmd/sidecars/sidecar_shim.go
+++ cmd/sidecars/sidecar_shim.go
@@ -36,6 +36,7 @@ import (
 	virtv1 "kubevirt.io/api/core/v1"
 	"kubevirt.io/client-go/log"
 
+	cloudinit "kubevirt.io/kubevirt/pkg/cloud-init"
 	"kubevirt.io/kubevirt/pkg/hooks"
 	hooksInfo "kubevirt.io/kubevirt/pkg/hooks/info"
 	hooksV1alpha1 "kubevirt.io/kubevirt/pkg/hooks/v1alpha1"
@@ -43,9 +44,11 @@ import (
 )
 
 const (
-	onDefineDomainLoggingMessage = "OnDefineDomain method has been called"
+	onDefineDomainLoggingMessage  = "OnDefineDomain method has been called"
+	preCloudInitIsoLoggingMessage = "PreCloudInitIso method has been called"
 
-	onDefineDomainBin = "onDefineDomain"
+	onDefineDomainBin  = "onDefineDomain"
+	preCloudInitIsoBin = "preCloudInitIso"
 )
 
 type infoServer struct {
@@ -55,7 +58,8 @@ type infoServer struct {
 func (s infoServer) Info(ctx context.Context, params *hooksInfo.InfoParams) (*hooksInfo.InfoResult, error) {
 	log.Log.Info("Info method has been called")
 	supportedHookPoints := map[string]string{
-		hooksInfo.OnDefineDomainHookPointName: onDefineDomainBin,
+		hooksInfo.OnDefineDomainHookPointName:  onDefineDomainBin,
+		hooksInfo.PreCloudInitIsoHookPointName: preCloudInitIsoBin,
 	}
 	hookPoints := []*hooksInfo.HookPoint{}
 
@@ -93,10 +97,15 @@ func (s v1Alpha2Server) OnDefineDomain(ctx context.Context, params *hooksV1alpha
 		DomainXML: newDomainXML,
 	}, nil
 }
+
 func (s v1Alpha2Server) PreCloudInitIso(_ context.Context, params *hooksV1alpha2.PreCloudInitIsoParams) (*hooksV1alpha2.PreCloudInitIsoResult, error) {
-	log.Log.Info("PreCloudInitIso method has been called")
+	log.Log.Info(preCloudInitIsoLoggingMessage)
+	cloudInitData, err := runPreCloudInitIso(params.GetVmi(), params.GetCloudInitData())
+	if err != nil {
+		return nil, err
+	}
 	return &hooksV1alpha2.PreCloudInitIsoResult{
-		CloudInitData: params.GetCloudInitData(),
+		CloudInitData: cloudInitData,
 	}, nil
 }
 
@@ -109,6 +118,35 @@ func (s v1Alpha1Server) OnDefineDomain(ctx context.Context, params *hooksV1alpha
 	return &hooksV1alpha1.OnDefineDomainResult{
 		DomainXML: newDomainXML,
 	}, nil
+}
+
+func runPreCloudInitIso(vmiJSON []byte, cloudInitDataJSON []byte) ([]byte, error) {
+	// Check binary exists
+	if _, err := exec.LookPath(preCloudInitIsoBin); err != nil {
+		return nil, fmt.Errorf("Failed in finding %s in $PATH: %s", preCloudInitIsoBin, err.Error())
+	}
+
+	// Validate params before calling hook script
+	vmiSpec := virtv1.VirtualMachineInstance{}
+	if err := json.Unmarshal(vmiJSON, &vmiSpec); err != nil {
+		log.Log.Reason(err).Errorf("Failed to unmarshal given VMI spec: %s", vmiJSON)
+		panic(err)
+	}
+
+	cloudInitData := cloudinit.CloudInitData{}
+	err := json.Unmarshal(cloudInitDataJSON, &cloudInitData)
+	if err != nil {
+		log.Log.Reason(err).Errorf("Failed to unmarshal given CloudInitData: %s", cloudInitDataJSON)
+		panic(err)
+	}
+
+	args := append([]string{},
+		"--vmi", string(vmiJSON),
+		"--cloud-init", string(cloudInitDataJSON))
+
+	log.Log.Infof("Executing %s", preCloudInitIsoBin)
+	command := exec.Command(preCloudInitIsoBin, args...)
+	return command.Output()
 }
 
 func runOnDefineDomain(vmiJSON []byte, domainXML []byte) ([]byte, error) {
