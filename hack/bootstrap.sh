dbe8bdb3f42a7359e3eb88c72298634f8cbce8c7
Andrea Bolognani <abologna@redhat.com>
2024-01-24 16:49:53 +0100

    rpm: Update virtualization packages
    Specifically:
    
           QEMU        8.0.0 -> 8.2.0
        libvirt        9.5.0 -> 10.0.0
        SeaBIOS       1.16.1 -> 1.16.3
          EDKII     20230524 -> 20231122
          passt     20230818 -> 20231204
      virtiofsd        1.7.2 -> 1.1.10.1
      guestfs-tools   1.50.1 -> 1.51.6
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>

--- hack/bootstrap.sh
+++ hack/bootstrap.sh
@@ -26,7 +26,7 @@ KUBEVIRT_NO_BAZEL=${KUBEVIRT_NO_BAZEL:-false}
 HOST_ARCHITECTURE="$(uname -m)"
 
 sandbox_root=${SANDBOX_DIR}/default/root
-sandbox_hash="79cda77c407489645571b68ea8e8e6f813b99fdd"
+sandbox_hash="a8284f03fb564fc46f80b6b74b38c9567eceaa92"
 
 function kubevirt::bootstrap::regenerate() {
     (
