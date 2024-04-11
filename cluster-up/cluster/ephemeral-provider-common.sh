438681bcb6c080f8c72ce983bf7c40ba9f42cc15
Victor Toso <victortoso@redhat.com>
2024-04-11 15:14:48 +0200

    do not merge: cluster-up: fixup
    Signed-off-by: Victor Toso <victortoso@redhat.com>

--- cluster-up/cluster/ephemeral-provider-common.sh
+++ cluster-up/cluster/ephemeral-provider-common.sh
@@ -75,7 +75,7 @@ master_ip=$(_main_ip)
 kubeconfig=${BASE_PATH}/$KUBEVIRT_PROVIDER/.kubeconfig
 kubectl=${BASE_PATH}/$KUBEVIRT_PROVIDER/.kubectl
 gocli=${BASE_PATH}/../cluster-up/cli.sh
-docker_prefix=\${DOCKER_PREFIX:-localhost:$(_port registry)/kubevirt}
+docker_prefix=\${DOCKER_PREFIX:-127.0.0.1:$(_port registry)/kubevirt}
 manifest_docker_prefix=\${DOCKER_PREFIX:-registry:5000/kubevirt}
 EOF
 }
