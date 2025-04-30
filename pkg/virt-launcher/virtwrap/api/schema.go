victortoso/kubevirt

commit 77ed9a63426db67011a41b3bb2617d1abba7f63f

virtwrap: handle ACPI SLIC table
Signed-off-by: Victor Toso <victortoso@redhat.com>

pkg/virt-launcher/virtwrap/api/schema.go
@@ -933,6 +933,7 @@ func (alias *Alias) UnmarshalJSON(data []byte) error {
 
 type OS struct {
 	Type       OSType    `xml:"type"`
+	ACPI       *OSACPI   `xml:"acpi,omitempty"`
 	SMBios     *SMBios   `xml:"smbios,omitempty"`
 	BootOrder  []Boot    `xml:"boot"`
 	BootMenu   *BootMenu `xml:"bootmenu,omitempty"`
@@ -948,6 +949,15 @@ type OSType struct {
 	OS      string `xml:",chardata"`
 	Arch    string `xml:"arch,attr,omitempty"`
 	Machine string `xml:"machine,attr,omitempty"`
+}
+
+type OSACPI struct {
+	Table ACPITable `xml:"table,omitempty"`
+}
+
+type ACPITable struct {
+	Path string `xml:",chardata"`
+	Type string `xml:"type,attr,omitempty"`
 }
 
 type SMBios struct {

pkg/virt-launcher/virtwrap/converter/converter.go
@@ -1368,6 +1368,18 @@ func Convert_v1_Firmware_To_related_apis(vmi *v1.VirtualMachineInstance, domain 
 		domain.Spec.OS.KernelArgs = firmware.KernelBoot.KernelArgs
 	}
 
+	if firmware.ACPI != nil {
+		if slicNameRef := firmware.ACPI.SlicNameRef; slicNameRef != "" {
+			path := fmt.Sprintf("/var/run/kubevirt-private/secret/%s/slic.bin", slicNameRef)
+			domain.Spec.OS.ACPI = &api.OSACPI{
+				Table: api.ACPITable{
+					Type: "slic",
+					Path: path,
+				},
+			}
+		}
+	}
+
 	return nil
 }
 

commit 47a0dfc8e340e180623eb316bc312cfdd518f043

virtwrap: fix ACPI table on schema.go
It should be possible to add multiple ACPI tables.
See libvirt-go-xml-module DomainACPI type [0].

Confirmed it working with e2e test that I'm actually fixing typo here
too.

[0] https://gitlab.com/libvirt/libvirt-go-xml-module/-/blob/master/domain.go#L2405

Fixes: 77ed9a6342 virtwrap: handle ACPI SLIC table
Signed-off-by: Victor Toso <victortoso@redhat.com>

pkg/virt-launcher/virtwrap/api/schema.go
@@ -998,7 +998,7 @@ type OSType struct {
 }
 
 type OSACPI struct {
-	Table ACPITable `xml:"table,omitempty"`
+	Table []ACPITable `xml:"table,omitempty"`
 }
 
 type ACPITable struct {

pkg/virt-launcher/virtwrap/converter/converter.go
@@ -1227,12 +1227,15 @@ func Convert_v1_Firmware_To_related_apis(vmi *v1.VirtualMachineInstance, domain 
 			return err
 		}
 
-		domain.Spec.OS.ACPI = &api.OSACPI{
-			Table: api.ACPITable{
+		if domain.Spec.OS.ACPI == nil {
+			domain.Spec.OS.ACPI = &api.OSACPI{}
+		}
+
+		domain.Spec.OS.ACPI.Table = append(domain.Spec.OS.ACPI.Table,
+			api.ACPITable{
 				Type: "slic",
 				Path: path,
-			},
-		}
+			})
 	}
 
 	return nil

tests/vmi_configuration_test.go
@@ -567,7 +567,7 @@ var _ = Describe("[sig-compute]Configurations", decorators.SigCompute, func() {
 		})
 
 		Context("with ACPI SLIC table", func() {
-			It("Should configure guest APCI SLIC with Secret file", func() {
+			It("Should configure guest ACPI SLIC with Secret file", func() {
 				const (
 					volumeSlicSecretName = "volume-slic-secret"
 					secretWithSlicName   = "secret-with-slic-data"